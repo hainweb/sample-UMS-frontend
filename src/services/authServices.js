b5c573ba5c2ddfa2ac2c921dc7b3ef89d2e70957
hain <hainofficialweb@gmail.com>
2025-05-07 13:18:04 +0530

    feat(auth): add auth services

---
+++ src/services/authServices.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const signup = async (formData) => {
+  try {
+    const response = await axios.post(`${API_URL}/auth/signup`, formData, {
+      withCredentials: true,
+    });
+
+    console.log("Signup response:", response);
+    return response;
+  } catch (error) {
+    console.error("Signup error:", error.response?.data || error.message);
+    return null;
+  }
+};
+
+export const login = async (formData) => {
+  try {
+    const response = await axios.post(`${API_URL}/auth/login`, formData, {
+      withCredentials: true,
+    });
+
+    console.log("Login response:", response);
+    return response;
+  } catch (error) {
+    console.error("Login error:", error.response?.data || error.message);
+    return null;
+  }
+};
