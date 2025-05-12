607eb6d93b6c9c8235954fc39c774e42fc61aae5
hain <hainofficialweb@gmail.com>
2025-05-12 16:38:58 +0530

    feat(auth): add Protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Signup from "./pages/auth/Signup/Signup";
 import Login from "./pages/auth/Login/Login";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoutes>
+                <Home />
+              </ProtectedRoutes>
+            }
+          />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
         </Routes>
---
+++ src/components/ProtectedRoutes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoutes = ({ children }) => {
+  const { user } = useAuth();
+ 
+  if (!user) {
+    return <Navigate to="/login" />
+  }
+  return children;
+};
+
+export default ProtectedRoutes;
