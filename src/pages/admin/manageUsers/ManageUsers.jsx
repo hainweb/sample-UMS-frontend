5e81d9bad1761e02c6aed3458440b2b367b803ea
hain <hainofficialweb@gmail.com>
2025-06-03 22:05:44 +0530

    fix: fix user manage

--- src/pages/admin/manageUsers/ManageUsers.jsx
+++ src/pages/admin/manageUsers/ManageUsers.jsx
@@ -1,25 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
+import { getAllUsers } from "../../../services/adminServices";
+import { useState } from "react";
 
 const ManageUsers = () => {
-  const users = [
-    { name: "Ajin", age: 18, email: "jfj", role: "user", createdAt: "54ghhh" },
-    {
-      name: "user 2",
-      age: 39,
-      email: "jfjsnj",
-      role: "user",
-      createdAt: "95n55n",
-    },
-  ];
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await getAllUsers();
+        console.log("Fetched users:", response);
+
+        setUsers(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchUsers();
+  }, []);
   return (
     <div>
       {users.map((user) => (
-        <>
+        <div key={user._id}>
           <h2>Name: {user.name}</h2>
           <h2>Age: {user.age}</h2>
           <h2>Email: {user.email}</h2>
           <p>Created: {user.createdAt}</p>
-        </>
+        </div>
       ))}
     </div>
   );
