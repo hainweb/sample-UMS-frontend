eae012ab23b66af14ae195b0f97d14a778e12e4d
hain <hainofficialweb@gmail.com>
2025-06-19 23:42:50 +0530

    fix: fix issues

--- src/App.jsx
+++ src/App.jsx
@@ -12,70 +12,81 @@ import AdminLayout from "./layout/AdminLayout";
 import ManageUsers from "./pages/admin/manageUsers/ManageUsers";
 import VerifyAdmin from "./components/VerifyAdmin";
 import UnAuthorized from "./pages/unAuthorize/UnAuthorized";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getUser } from "./services/userServices";
 import { useAuth } from "./context/AuthContext";
 
 function App() {
   const { login } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const verifyUser = async () => {
       try {
         const response = await getUser();
+        console.log("verifictaio", response);
+
         if (response.success) {
           login(response.user);
         }
       } catch (error) {
         console.log(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     verifyUser();
   }, []);
   return (
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoutes>
-                <Home />
-              </ProtectedRoutes>
-            }
-          />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoutes>
-                <Profile />
-              </ProtectedRoutes>
-            }
-          />
-        </Route>
-        <Route element={<AdminLayout />}>
-          <Route
-            path="/admin"
-            element={
-              <VerifyAdmin>
-                <AdminHome />
-              </VerifyAdmin>
-            }
-          />
-          <Route
-            path="/manage-users"
-            element={
-              <VerifyAdmin>
-                <ManageUsers />
-              </VerifyAdmin>
-            }
-          />
-        </Route>
-        <Route path="/unauthorized" element={<UnAuthorized />} />
-      </Routes>
-    </Router>
+    <>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <Router>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoutes>
+                    <Home />
+                  </ProtectedRoutes>
+                }
+              />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoutes>
+                    <Profile />
+                  </ProtectedRoutes>
+                }
+              />
+            </Route>
+            <Route element={<AdminLayout />}>
+              <Route
+                path="/admin"
+                element={
+                  <VerifyAdmin>
+                    <AdminHome />
+                  </VerifyAdmin>
+                }
+              />
+              <Route
+                path="/manage-users"
+                element={
+                  <VerifyAdmin>
+                    <ManageUsers />
+                  </VerifyAdmin>
+                }
+              />
+            </Route>
+            <Route path="/unauthorized" element={<UnAuthorized />} />
+          </Routes>
+        </Router>
+      )}
+    </>
   );
 }
 
