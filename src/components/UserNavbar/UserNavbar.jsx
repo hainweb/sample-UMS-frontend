85736da50d7c712e805f73a778f0cce1caa5a993
hain <hainofficialweb@gmail.com>
2025-06-21 23:48:36 +0530

    feat(user): add logout in navbar

--- src/components/UserNavbar/UserNavbar.jsx
+++ src/components/UserNavbar/UserNavbar.jsx
@@ -2,7 +2,7 @@ import { useAuth } from "../../context/AuthContext";
 import "./UserNavbar.css";
 import { useNavigate } from "react-router-dom";
 const UserNavbar = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
   const navigate = useNavigate();
   return (
     <div>
@@ -11,7 +11,16 @@ const UserNavbar = () => {
           Profile manager
         </h2>
         <div className="sub-navlink">
-          <h3 className="user-name">{user?.name}</h3>
+          {user ? (
+            <h3 onClick={() => logout()} className="user-name">
+              {user?.name} /logout
+            </h3>
+          ) : (
+            <h3 className="login" onClick={() => navigate("/login")}>
+              Login
+            </h3>
+          )}
+
           {user?.role == "admin" && (
             <h3 className="sub-heading" onClick={() => navigate("/admin")}>
               Admin Panel
